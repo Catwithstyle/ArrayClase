c0f64a2003abed6b69306cc61ad435537c3ef211
Ruben <rubenernestovalle@gmail.com>
2024-09-20 11:03:15 -0600

    Ejercicio1

---
+++ claseArray/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int tan = 10;
+int sumaPar = 0;
+
+int sumaImpar = 0;
+int[] numeros = new int[tan];
+Console.WriteLine("Digite los datos del arreglo: ");
+Console.WriteLine();
+for (int i = 0; i < tan; i++)
+{
+    Console.WriteLine("Ingresa un numero: ");
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+    if (numeros[i] % 2 == 0)
+    {
+            sumaPar += numeros[i];
+    }else  sumaImpar += numeros[i];
+    
+}
+
+Console.WriteLine();
+Console.WriteLine("Datos del arreglo");
+Console.WriteLine();
+for (int i = 0; i < tan; i++)
+{
+   Console.Write(numeros[i] + ", "); 
+}
+Console.WriteLine();
+Console.WriteLine($"La suma de los pares es: {sumaPar}");
+Console.WriteLine($"La suma de los numeros impares es {sumaImpar}");
+
+Console.ReadKey();
